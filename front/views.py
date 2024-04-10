6f8be86ccc6562fd0904b63b99dd91761638b611
Denis Tauchman <denis.tauchman@gmail.com>
2024-04-10 22:26:52 +0200

    data handling

--- front/views.py
+++ front/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from front.data_handler import get_current_weather
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    current_weather = get_current_weather(50.77, 15.05)
+    return JsonResponse(current_weather.json(), safe=False)
